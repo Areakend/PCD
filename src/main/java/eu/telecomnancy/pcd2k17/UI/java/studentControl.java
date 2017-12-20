20f185f6cd24f1f314ffb89521f78be7ab3449da
Raphael Ohanian <raphael.ohanian@telecomnancy.eu>
2017-12-20 12:57:06 +0100

    Ajout d'une option pour choisir une matiere / projet

--- src/main/java/eu/telecomnancy/pcd2k17/UI/java/studentControl.java
+++ src/main/java/eu/telecomnancy/pcd2k17/UI/java/studentControl.java
@@ -3,16 +3,22 @@ package eu.telecomnancy.pcd2k17.UI.java;
 import javafx.scene.control.TextArea;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import eu.telecomnancy.pcd2k17.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.text.*;
 
 public class studentControl {
 
@@ -27,20 +33,61 @@ public class studentControl {
 
 	@FXML
 	TextArea fileList = new TextArea();
+	
+	@FXML
+	Text matiereText = new Text();
+	
+	@FXML	
+	Text Title = new Text();
+	
+	@FXML
+	Text HeureRendu = new Text();
+	
+	@FXML
+	Text DateRendu = new Text();
 
 	@FXML
 	Label test;
+	
+	static String matiereChoisie = null;
 
+	static String projetChoisi = null;
+	
+	public void choisirMatiere(ActionEvent event) throws IOException {
+		
+		List<String> matiere = new LinkedList<>();
+		matiere.add("PCD");
+		matiere.add("MOCI");
+
+		ChoiceDialog<String> dialog = new ChoiceDialog<>(" ", matiere);
+		dialog.setTitle("Choix de la matière");
+		dialog.setHeaderText(null);
+		dialog.setContentText("Matières :");
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()) {
+			matiereChoisie = result.get();
+			List<String> projets = new LinkedList<>();
+				projets.add("Projet 1");
+				projets.add("Projet 2");
+				projets.add("Projet 3");
+			
+			ChoiceDialog<String> dialog1 = new ChoiceDialog<>(" ", projets);
+			dialog1.setTitle("Choix du projet");
+			dialog1.setHeaderText(null);
+			dialog1.setContentText("Projet(s) de " + matiereChoisie + ": ");
+			Optional<String> result2 = dialog1.showAndWait();
+			if (result2.isPresent()) {
+				projetChoisi = result2.get();
+				matiereText.setText(matiereChoisie);
+				Title.setText(projetChoisi);
+			}	
+		}
+	}
 	@FXML
 	public void validDevoir(ActionEvent event) throws IOException {
-		
-		
-		/*
-		 * Stage FileLoaderStage = (Stage) ((Node)
-		 * event.getSource()).getScene().getWindow();
-		 * FileLoaderStage.setScene(new Scene(Main.createDevoir, 1600, 800));
-		 * FileLoaderStage.show();
-		 */
+
 		log.debug("Connexion button was clicked!");
 		log.debug("Input CommitText : " + commitMessage.getText());
 
