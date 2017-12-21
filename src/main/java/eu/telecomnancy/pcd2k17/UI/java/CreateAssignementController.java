4806a72b3461af67ab351c65b826470ea04949fd
Raphael Ohanian <raphael.ohanian@telecomnancy.eu>
2017-12-21 14:11:47 +0100

    Ajout d'une fonction "Ajout de matiere", il reste le bouton a implementer sur l'IG

--- src/main/java/eu/telecomnancy/pcd2k17/UI/java/CreateAssignementController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/UI/java/CreateAssignementController.java
@@ -1,8 +1,12 @@
 package eu.telecomnancy.pcd2k17.UI.java;
 
-
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import eu.telecomnancy.pcd2k17.Main;
@@ -14,135 +18,165 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextInputDialog;
 
-
-public class CreateAssignementController{
+public class CreateAssignementController {
 
 	final static Logger log = LogManager.getLogger(ConnexionScreenController.class);
-	private String discipline,title,description,heureFin,isPrivate;
+	private String discipline, title, description, heureFin, isPrivate;
 
-	private LocalDate releaseDate,deadline;
-	
+	private LocalDate releaseDate, deadline;
+
 	private int NbEleves;
-	
-	  ObservableList<String> list = FXCollections.observableArrayList("1","2");
-	
-	  @SuppressWarnings("rawtypes")
+
+	ObservableList<String> list = FXCollections.observableArrayList("1", "2");
+
+	@SuppressWarnings("rawtypes")
 	@FXML
-	  ChoiceBox matiereChoiceBox = new ChoiceBox();
-	  
-	  @FXML
-	  TextField titreTextField = new TextField();
-	  
-	  @FXML
-	  TextField prefixTextField = new TextField();
-	  
-	  @FXML
-	  TextArea descriptionText = new TextArea();
-	  
-	  @FXML
-	  DatePicker releaseDatePicker = new DatePicker();
-	  
-	  @FXML
-	  DatePicker endDatePicker = new DatePicker();
-	  
-	  @FXML
-	  TextField endHour = new TextField();
-	  
-	  @FXML
-	  RadioButton radioPrivate; 
-	  
-	  @FXML
-	  RadioButton radioPublic;  
-	  
-	  @FXML
-	  TextField NbElevesTextField = new TextField();
-	 
-	  @FXML
-	  public void CreateDevoir(ActionEvent event) throws IOException {
-		  /*Stage FileLoaderStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		    FileLoaderStage.setScene(new Scene(Main.createDevoir, 1600, 800));
-		    FileLoaderStage.show();*/
-		  boolean error = false;
-		  if ( titreTextField.getText().length() == 0 || prefixTextField.getText().length() == 0||descriptionText.getText().length() == 0 
-				  || releaseDatePicker.getValue() == null || endDatePicker.getValue() == null || endHour.getText().length() == 0
-				  ||NbElevesTextField.getText().length() == 0){
-			  Alert alert = new Alert(AlertType.WARNING);
-		        alert.setTitle("Formulaire(s) vide(s)");
-		        alert.setHeaderText("Formulaire(s) vide(s).");
-		        alert.setContentText("Veuillez remplir tous les formulaires.");
+	ChoiceBox matiereChoiceBox = new ChoiceBox();
 
-		        alert.showAndWait();
-		  } else if(!endHour.getText().matches("\\d{1,2}:\\d{1,2}")) {
-			  Alert alert = new Alert(AlertType.ERROR);
-		        alert.setTitle("Erreur de format");
-		        alert.setHeaderText("Erreur de format");
-		        alert.setContentText("Le format HH:MM est attendu dans le champ : Heure de rendu.");
-		        alert.showAndWait();
-		        error = true;    
-		  } else if(releaseDatePicker.getValue().isAfter(endDatePicker.getValue())) {
-			  Alert alert = new Alert(AlertType.ERROR);
-		        alert.setTitle("Erreur de date");
-		        alert.setHeaderText("Erreur de date");
-		        alert.setContentText("La date de rendu doit ï¿½tre aprï¿½s la date de dï¿½but.");
-		        alert.showAndWait();
-		        error = true;    
-		  }else{
-			  try {
-					Integer.parseInt(NbElevesTextField.getText());
-				} catch (NumberFormatException e){
-					Alert alert = new Alert(AlertType.ERROR);
-			        alert.setTitle("Erreur de type");
-			        alert.setHeaderText("Erreur de type");
-			        alert.setContentText("Un entier est attendu dans le champ : Nb d'ï¿½lï¿½ve/groupe.");
-			        alert.showAndWait();
-			        error = true;
-				}
-			  if(!error){
-				  discipline = matiereChoiceBox.getValue().toString();
-				  title = titreTextField.getText();
-				  description = descriptionText.getText();
-				  releaseDate = releaseDatePicker.getValue();
-				  deadline = endDatePicker.getValue();
-				  heureFin = endHour.getText();
-				  isPrivate = radioPrivate.isSelected() ? "true" : "false";
-				  NbEleves = Integer.parseInt(NbElevesTextField.getText());
-				  Main.mainPane.setCenter(Main.panel4);
-			  }
-			  
-		  }
-		  
+	@FXML
+	TextField titreTextField = new TextField();
 
-		  log.debug("Create Devoir");
-		  log.debug("isPrivate : " + isPrivate);
-		  log.debug("Matiere : " +discipline);
-		  log.debug("Titre : " + title);
-		  log.debug("Text : " + description);
-		  log.debug("ReleaseDate : " + releaseDate);
-		  log.debug("EndDate : " + deadline + " ï¿½ " + heureFin+"h\n");
+	@FXML
+	TextField prefixTextField = new TextField();
 
-		  
-	  }
-	  
+	@FXML
+	TextArea descriptionText = new TextArea();
 
-	  @FXML
-	  public void back2Menu(ActionEvent event) throws IOException {
+	@FXML
+	DatePicker releaseDatePicker = new DatePicker();
 
-			Main.mainPane.setCenter(Main.panel1);
-		  
-	  }
-	  
-	  
+	@FXML
+	DatePicker endDatePicker = new DatePicker();
+
+	@FXML
+	TextField endHour = new TextField();
+
+	@FXML
+	RadioButton radioPrivate;
+
+	@FXML
+	RadioButton radioPublic;
+
+	@FXML
+	TextField NbElevesTextField = new TextField();
+
+	@FXML
+	public void CreateDevoir(ActionEvent event) throws IOException {
+		/*
+		 * Stage FileLoaderStage = (Stage) ((Node)
+		 * event.getSource()).getScene().getWindow();
+		 * FileLoaderStage.setScene(new Scene(Main.createDevoir, 1600, 800));
+		 * FileLoaderStage.show();
+		 */
+		boolean error = false;
+		if (titreTextField.getText().length() == 0 || prefixTextField.getText().length() == 0
+				|| descriptionText.getText().length() == 0 || releaseDatePicker.getValue() == null
+				|| endDatePicker.getValue() == null || endHour.getText().length() == 0
+				|| NbElevesTextField.getText().length() == 0) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Formulaire(s) vide(s)");
+			alert.setHeaderText("Formulaire(s) vide(s).");
+			alert.setContentText("Veuillez remplir tous les formulaires.");
+
+			alert.showAndWait();
+		} else if (!endHour.getText().matches("\\d{1,2}:\\d{1,2}")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erreur de format");
+			alert.setHeaderText("Erreur de format");
+			alert.setContentText("Le format HH:MM est attendu dans le champ : Heure de rendu.");
+			alert.showAndWait();
+			error = true;
+		} else if (releaseDatePicker.getValue().isAfter(endDatePicker.getValue())) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Erreur de date");
+			alert.setHeaderText("Erreur de date");
+			alert.setContentText("La date de rendu doit ï¿½tre aprï¿½s la date de dï¿½but.");
+			alert.showAndWait();
+			error = true;
+		} else {
+			try {
+				Integer.parseInt(NbElevesTextField.getText());
+			} catch (NumberFormatException e) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Erreur de type");
+				alert.setHeaderText("Erreur de type");
+				alert.setContentText("Un entier est attendu dans le champ : Nb d'ï¿½lï¿½ve/groupe.");
+				alert.showAndWait();
+				error = true;
+			}
+			if (!error) {
+				discipline = matiereChoiceBox.getValue().toString();
+				title = titreTextField.getText();
+				description = descriptionText.getText();
+				releaseDate = releaseDatePicker.getValue();
+				deadline = endDatePicker.getValue();
+				heureFin = endHour.getText();
+				isPrivate = radioPrivate.isSelected() ? "true" : "false";
+				NbEleves = Integer.parseInt(NbElevesTextField.getText());
+				Main.mainPane.setCenter(Main.panel4);
+			}
+
+		}
+
+		log.debug("Create Devoir");
+		log.debug("isPrivate : " + isPrivate);
+		log.debug("Matiere : " + discipline);
+		log.debug("Titre : " + title);
+		log.debug("Text : " + description);
+		log.debug("ReleaseDate : " + releaseDate);
+		log.debug("EndDate : " + deadline + " ï¿½ " + heureFin + "h\n");
+
+	}
+
+	@FXML
+	public void ajoutMatiere(ActionEvent event) throws IOException {
+		TextInputDialog dialog = new TextInputDialog("");
+		dialog.setTitle("Ajouter une matière");
+		dialog.setHeaderText(null);
+		dialog.setContentText("Nom de la matière :");
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Information");
+			alert.setHeaderText(null);
+			alert.setContentText("Veuillez choisir une liste d'élèves");
+
+			alert.showAndWait();
+			final FileChooser fileChooser = new FileChooser();
+			fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV", "*.csv"),
+					new FileChooser.ExtensionFilter("ALL", "*.*"));
+
+			Stage FileLoaderStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			File file = fileChooser.showOpenDialog(FileLoaderStage);
+			String path = file.getAbsolutePath();
+
+			System.out.println("File : " + path);
+		}
+	}
+
+	@FXML
+	public void back2Menu(ActionEvent event) throws IOException {
+
+		Main.mainPane.setCenter(Main.panel1);
+
+	}
 
 	@SuppressWarnings("unchecked")
 	@FXML
-	  public void initialize(){
-		  matiereChoiceBox.setItems(list);
-		  matiereChoiceBox.setValue("1"); 
-		  radioPrivate.setSelected(true);
-	  }
-	
+	public void initialize() {
+		matiereChoiceBox.setItems(list);
+		matiereChoiceBox.setValue("1");
+		radioPrivate.setSelected(true);
+	}
+
 }
